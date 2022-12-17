e935697982af58464a3ccbbb1202f88588dd64ad
Pichet Thongsamrit <68932703+smilejung@users.noreply.github.com>
2022-12-17 14:50:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<script language="JavaScript1.2">
+/*
+Fading Image Script 
+Dynamic Drive (www.dynamicdrive.com)
+*/
+
+function makevisible(cur,which){
+  if (which==0)
+    cur.filters.alpha.opacity=100
+  else
+    cur.filters.alpha.opacity=50
+}
+</script>
+<table border="0" cellpadding="0" cellspacing="0" width="<?=$widthSUM;?>" align="center">
+	<tbody>
+		<tr>
+			<td>
+				<ul>
+					<li>
+						<a href="sendmail" target="_blank">ส่งอีเมล</a></li><br>
+					<li>
+						<a href="random" target="_blank">อัพรูปภาพ</a></li><br>
+					<li>
+						<a href="admin" target="_blank">กลับหน้า admin</a></li><br>
+					<li>
+						<a href="http://rayonghealth.com/cdc/index.php?name=admin" target="_blank">กลับหน้าหลัก admin</a></li><br>
+					</li>
+				</ul>
+			</td>
+		</tr>
+	</tbody>
+</table>
